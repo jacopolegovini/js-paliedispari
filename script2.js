2477883daadaeb7808cee6d8711c0f413813222b
jacopolegovini <legovini96@gmail.com>
2024-08-22 16:06:21 +0200

    draft 2 js added

---
+++ script2.js
@@ -0,0 +1,31 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+// Recupera elementi dal DOM
+// Crea le variabili
+// Crea la funzione per generare un numero casuale
+//     Adopera la formula random 
+// Crea la funzione per stabilire se la somma dei due numeri sia pari o dispari
+//     Aggiungi la somma come parametro della funzione
+//     Con la somma usa l'operatore modulo
+// Aggiungi evento al bottone
+// Inserisci le funzioni
+// Dichiara il vincitore
+
+
+// Recupera elementi dal DOM
+const evenElement = document.getElementById('even');
+const oddElement = document.getElementById('odd');
+const numberElement = document.getElementById('number');
+const buttonWinnerElement = document.getElementById('winner');
+const resultElement = document.querySelector('p');
+
+// Crea le variabili
+
+
+// Crea la funzione per generare un numero casuale
+
