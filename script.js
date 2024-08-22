ef134a7f04e58afab4c331d90221e738a3330589
jacopolegovini <legovini96@gmail.com>
2024-08-22 14:59:41 +0200

    draft js added

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+// Recupera elementi dal DOM
+// Crea funzione
+    // Crea array con la parola immessa
+    // Controlla se la prima lettera corrisponde all'ultima e poi prosegui con le altre
+    // Ritorna il risultato in booleano
+// Aggiungi event.Listener per il bottone
+// Stampa il risultato
+
+
+// Recupera elementi dal DOM
+const word = document.getElementById('word');
+const result = document.getElementById('result');
+
+
+// Crea funzione
+/**
+ * This function verify if the value "word" is palindrome
+ * @param {string} word 
+ * @returns boolean
+ */
+function isPalindrome(word) {
+    return false;
+}
